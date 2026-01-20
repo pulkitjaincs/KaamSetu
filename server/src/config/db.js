562ae008baaa25e46f9a51795dbc8cf142168fb3
Pulkit Jain <pulkitjain.cse@gmail.com>
2026-01-20 08:43:13 +0530

    Initialize server project with basic dependencies and MongoDB connection setup.

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected");
+        console.log("MongoDB Connected with KaamSetuDB");
     }catch(error){
         console.log(error);
         process.exit(1);
